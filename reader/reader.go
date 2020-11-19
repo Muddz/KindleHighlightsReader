6bc8d12096d795bf1322b74e8980ac922b02caa2
Muddz <muddzdk@gmail.com>
2020-11-19 23:32:38 +0100

    Implementation of the highlight file reader package

---
+++ reader/reader.go
@@ -0,0 +1,17 @@
+package reader
+
+//Todo 1) Check if file is present
+//Todo 2) Check if file contains texts or is a .text
+//Todo 3) Open file and copy content to a string
+//Todo 4) Use regex to detect every part of the string and create Highlight[] objects
+//Todo 5) Close i/o file handler
+
+func ReadHighlightFile(path string) string {
+
+	return ""
+}
+
+func doExists(path string) bool {
+
+	return true
+}
---
+++ reader/reader_test.go
@@ -0,0 +1,7 @@
+package reader
+
+import "testing"
+
+func TestRemoveWhiteSpaces(t *testing.T) {
+
+}
