fd9bef590a268c5e9ab161e6ccfadbd7a7394112
Muddz <muddzdk@gmail.com>
2020-11-23 00:38:08 +0100

    Implementation of save functionality

--- save/save.go
+++ save/save.go
@@ -10,38 +10,81 @@ import (
 )
 
 //TODO 1) Remove BOM and /r characters
+//func ToJSON(highlights []reader.Highlight, destination string) bool {
+//	b, err := json.Marshal(highlights)
+//	if err != nil {
+//		log.Println(err)
+//	}
+//	filename := fmt.Sprintf("%s/%s", destination, "My Clippings Json.txt")
+//	if f := createNewFile(filename); f != nil {
+//		return writeToFile(f, b)
+//	}
+//	return false
+//}
+
 func ToJSON(highlights []reader.Highlight, destination string) bool {
-	b, err := json.Marshal(highlights)
-	if err != nil {
-		log.Println(err)
-	}
 	filename := fmt.Sprintf("%s/%s", destination, "My Clippings Json.txt")
+	b := convertToJSON(highlights)
 	if f := createNewFile(filename); f != nil {
 		return writeToFile(f, b)
 	}
 	return false
 }
 
+func convertToJSON(highlights []reader.Highlight) []byte {
+	b, err := json.Marshal(highlights)
+	if err != nil {
+		log.Println(err)
+	}
+	return b
+}
+
+//func ToTxt(highlights []reader.Highlight, destination string) bool {
+//	layout := "%s, %s\n\n%s\n_______________________________\n\n"
+//	sb := strings.Builder{}
+//	for _, v := range highlights {
+//		highlight := fmt.Sprintf(layout, v.Author, v.Title, v.Text)
+//		sb.WriteString(highlight)
+//	}
+//	filename := fmt.Sprintf("%s/%s", destination, "My Clippings text.txt")
+//	if f := createNewFile(filename); f != nil {
+//		return writeToFile(f, []byte(sb.String()))
+//	}
+//	return false
+//}
+
 func ToTxt(highlights []reader.Highlight, destination string) bool {
+	txt := convertToText(highlights)
+	filename := fmt.Sprintf("%s/%s", destination, "My Clippings text.txt")
+	if f := createNewFile(filename); f != nil {
+		return writeToFile(f, []byte(txt))
+	}
+	return false
+}
+
+func convertToText(highlights []reader.Highlight) string {
 	layout := "%s, %s\n\n%s\n_______________________________\n\n"
 	sb := strings.Builder{}
 	for _, v := range highlights {
 		highlight := fmt.Sprintf(layout, v.Author, v.Title, v.Text)
 		sb.WriteString(highlight)
 	}
-	filename := fmt.Sprintf("%s/%s", destination, "My Clippings text.txt")
-	if f := createNewFile(filename); f != nil {
-		return writeToFile(f, []byte(sb.String()))
-	}
-	return false
+	return sb.String()
 }
 
 func toPDF() {
 
 }
 
-func toCSV() {
+func toCSV(highlights []reader.Highlight, destination string) {
 
+	if f := createNewFile(filename); f != nil {
+		return writeToFile(f, []byte(txt))
+	}
+}
+
+func convertToCSV(highlights []reader.Highlight) string {
+	return ""
 }
 
 func writeToFile(f *os.File, b []byte) bool {
