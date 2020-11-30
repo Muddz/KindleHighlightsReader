8d9635656139bc80ed3029a4c4efe3cfa2de596c
Muddz <muddzdk@gmail.com>
2020-11-30 11:09:56 +0100

    Implemented tests for save

--- save/save_test.go
+++ save/save_test.go
@@ -8,19 +8,51 @@ import (
 )
 
 func TestToJSON(t *testing.T) {
-
+	filename := getUserDesktopPath() + "\\My Clippings JSON.txt"
+	ok := ToJSON(nil)
+	if !ok {
+		t.Error("Failed to save to Json. Result was not ok")
+	}
+	if !fileExist(filename) {
+		t.Error("Failed to find JSON file: ", filename)
+	}
+	_ = os.Remove(filename)
 }
 
 func TestToTxt(t *testing.T) {
-
+	filename := getUserDesktopPath() + "\\My Clippings Text.txt"
+	ok := ToTxt(nil)
+	if !ok {
+		t.Error("Failed to save to Text. Result was not ok")
+	}
+	if !fileExist(filename) {
+		t.Error("Failed to find Text file: ", filename)
+	}
+	_ = os.Remove(filename)
 }
 
 func TestToPDF(t *testing.T) {
-
+	filename := getUserDesktopPath() + "\\My Clippings.pdf"
+	ok := ToPDF(nil)
+	if !ok {
+		t.Error("Failed to save to PDF. Result was not ok")
+	}
+	if !fileExist(filename) {
+		t.Error("Failed to find PDF file: ", filename)
+	}
+	_ = os.Remove(filename)
 }
 
 func TestToCSV(t *testing.T) {
-
+	filename := getUserDesktopPath() + "\\My Clippings.csv"
+	ok := ToCSV(nil)
+	if !ok {
+		t.Error("Failed to save to csv. Result was not ok")
+	}
+	if !fileExist(filename) {
+		t.Error("Failed to find csv file: ", filename)
+	}
+	_ = os.Remove(filename)
 }
 
 func TestSave(t *testing.T) {
@@ -48,7 +80,7 @@ func fileExist(path string) bool {
 	if os.IsNotExist(err) {
 		return false
 	}
-	return info.IsDir()
+	return !info.IsDir()
 }
 
 func TestCreateNewFile(t *testing.T) {
@@ -71,7 +103,6 @@ func TestWriteToFile(t *testing.T) {
 	if !ok {
 		t.Error("Failed to write ''Hello World' to file:", filename)
 	}
-
 	_ = f.Close()
 	_ = os.Remove(filename)
 }
